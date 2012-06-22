efb2b678266850649e590ccdb1fff9c7c1ee507b
bulanlily <bulanlily@gmail.com>
2012-06-22 15:11:29 +0800

    Add test and implement for Rover init and turn function

--- spec/src/rover_spec.rb
+++ spec/src/rover_spec.rb
@@ -2,12 +2,36 @@ require "rspec"
 require_relative '../../src/rover'
 
 describe Rover do
+  describe :initialize do
+    describe "when get a valid direction" do
+      it "should get the right direction" do
+        Rover.new(0, 0, 'N').direction.should == 'N'
+      end
+    end
+    describe "when get a invalid direction" do
+      it "should raise an exception" do
+        expect { Rover.new(0, 0, 'A') }.should raise_exception
+      end
+    end
+  end
+
   describe :turn do
-    let(:rover){Rover.new(0,0,'N')}
+    let(:rover) { Rover.new(0, 0, 'N') }
     describe "when get a left command" do
       it "should turn left" do
-         rover.turn('L')
-         rover.direction.should == 'W'
+        rover.turn('L')
+        rover.direction.should == 'W'
+      end
+    end
+    describe "when get a right command" do
+      it "should turn left" do
+        rover.turn('R')
+        rover.direction.should == 'E'
+      end
+    end
+    describe "when get a invalid command" do
+      it "should raise an exception" do
+        expect { rover.turn('A') }.should raise_exception
       end
     end
   end
--- src/rover.rb
+++ src/rover.rb
@@ -1,7 +1,20 @@
 class Rover
-  attr_accessor :x, :y, :direction, :plateau
+  DIRECTIONS = %w{W N E S}
+  attr_accessor :x, :y, :direction
 
-  def initialize x, y, direction
-    x, y, direction=x, y, direction
+  def initialize (x, y, direction)
+    raise"Invalid Direction Input!" unless DIRECTIONS.include? direction
+    @x, @y, @direction=x, y, direction
+  end
+
+  def turn action
+    @direction=case action
+                 when 'L'
+                   DIRECTIONS.fetch(DIRECTIONS.index(direction)-1, DIRECTIONS[0])
+                 when 'R'
+                   DIRECTIONS.fetch(DIRECTIONS.index(direction)+1, DIRECTIONS[0])
+                 else
+                   raise "Invalid Action Command!"
+               end
   end
 end
